96ca989a0504c8335c6c36dfe16109d349e4fb8b
Alan Kwok <akwok@splunk.com>
2018-09-25 21:35:25 -0700

    test

--- bot.js
+++ bot.js
@@ -25,8 +25,8 @@ function postMessage() {
 
   botResponse = cool();
   bibleverses.retrievePassage('Romans 2:3-4')
-    .then(response => response.json())
-    .then(data => verseResponse = data);
+    .then(response => response = response.json())
+    .then(response => verseResponse = response);
 
   options = {
     hostname: 'api.groupme.com',
