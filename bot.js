ee87e05f3a175845030aa705c3bb4bed36eeca08
Alan Kwok <akwok@splunk.com>
2018-10-03 15:09:27 -0700

    stuff

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ function getESVpassage(passage) {
         console.log(returnVerse);
         postMessageVerse(returnVerse);
       } else {
-        postMessageVerse("Error with verse " + passage);
+        postMessageError("Error with verse " + passage);
       };
     });
 
@@ -118,6 +118,41 @@ function postMessageVerse(passagetext) {
   body = {
     "bot_id" : botID,
     "text" : verseResponse + botResponse
+    // "text": botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessageErr(errorString) {
+  var botResponse, options, body, botReq;
+  botResponse = errorString;
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
     // "text": botResponse
   };
 
