aa42b8aae5f15188eb5f65f952728b3b3fc69a9d
Alan Kwok <akwok@splunk.com>
2018-10-03 14:26:01 -0700

    stuff

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    getESVpassage('Romans 2:3-4');
     this.res.end();
   } else {
     console.log("don't care");
@@ -21,7 +21,7 @@ function respond() {
   }
 }
 
-function getESVpassage(passage) {
+function getESVpassage(passage,) {
   returnVerse = ""; 
   console.log(crosswayAPIToken);
 
@@ -62,6 +62,7 @@ function getESVpassage(passage) {
       console.log(obj);
       returnVerse += obj['passages'][0];
       console.log(returnVerse);
+      postMessageVerse(returnVerse);
     });
 
   // ESVreq = HTTPS.request(options, function(res) {
@@ -94,6 +95,45 @@ function getESVpassage(passage) {
 
   
   return returnVerse;
+}
+
+function postMessageVerse(passagetext) {
+  var botResponse, options, body, botReq;
+  var verseResponse;
+
+  botResponse = cool();
+  verseResponse = getESVpassage('Romans 2:3-4');
+  console.log(verseResponse);
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : verseResponse + botResponse
+    // "text": botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
 }
 
 function postMessage() {
