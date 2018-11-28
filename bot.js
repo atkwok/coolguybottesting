69f76ce97e1c4ec1c32d43fc70152623f5a95b84
Alan Kwok <akwok@splunk.com>
2018-11-28 15:16:17 -0800

    new group

--- bot.js
+++ bot.js
@@ -14,33 +14,40 @@ var last_chunk_dict = {"44506327": "",
                   "42096063": "",
                   "31816708": "",
                   "15516149": "",
+                  "44909760": "",
                   "41805466": ""};
 var rest_of_passage_dict = {"44506327": [],
                   "42096063": [],
                   "31816708": [],
                   "15516149": [],
+                  "44909760": [],
                   "41805466": []};
 var botID_dict = {"44506327": process.env.TEST_ID,
                   "42096063": process.env.BOT_ID,
                   "31816708": process.env.TEST_TWO_ID,
                   "15516149": process.env.PEER_BOT_ID,
+                  "44909760": process.env.SF_LUNCH_BOT_ID,
                   "41805466": process.env.PRAXIS_BOT_ID};
 var rateLimit = {"44506327": 50,
                   "42096063": 5,
                   "31816708": 10,
                   "15516149": 5,
+                  "44909760": 5,
                   "41805466": 5};
 var rateLimitTimes = {"44506327": [],
                   "42096063": [],
                   "31816708": [],
                   "15516149": [],
+                  "44909760": [],
                   "41805466": []};
 
 const hangout_question = "Would anyone like to video chat the next time when it reaches noon PST? If so, please like this message!\n\n- WellVersedBot";
 const rate_limit_message = "Error 429! Too many requests! Please stop spamming me and wait a few minutes >_<";
+const help_message = "Hello! I'm Verse Bot!\nYou can tell me to do the following:\n/verse [reference] - the text of the verse based on the reference\n/dt - today's DT passage!\n/proverb - a random proverb\n/memory - a random passage from 100 verses every christian should know\n/romans - a random romans memory verse\n/values - a random core values verse\n\nI also have the following features! I rest on Sundays and at nights, and I promise to not send more than 5 messages a minute!\n\nPlease don't spam me! I'm only here to be helpful ^_^"
 var romans_memory_verses = ["Romans 1:14", "Romans 1:15", "Romans 1:16-17", "Romans 1:21-22", "Romans 2:3-4", "Romans 3:10-12", "Romans 3:22-24", "Romans 4:2-3", "Romans 4:6-8", "Romans 5:1-2", "Romans 5:6-8", "Romans 5:9-10", "Romans 6:11-12", "Romans 6:13-14", "Romans 6:23", "Romans 7:18-19", "Romans 7:24-25", "Romans 8:1", "Romans 8:15-16", "Romans 8:17-18", "Romans 8:22-23", "Romans 8:26", "Romans 8:28", "Romans 8:31-34", "Romans 8:35-37", "Romans 8:38-39", "Romans 9:1-3", "Romans 10:9-10", "Romans 10:13-15", "Romans 10:17", "Romans 11:33-36", "Romans 12:1-3", "Romans 12:4-5", "Romans 12:9-10", "Romans 12:11-14", "Romans 12:15-18", "Romans 12:21", "Romans 13:7", "Romans 13:8", "Romans 13:9-10", "Romans 13:14", "Romans 14:7-8", "Romans 14:11-13", "Romans 15:1-3", "Romans 16:17-18", "Romans 16:25-27"];
 var hundred_memory_verses = ["Genesis 1:1", "Deuteronomy 6:6-7", "Joshua 1:8", "2 Chronicles 7:14", "Psalm 4:8", "Psalm 23:1-6", "Psalm 55:22", "Psalm 56:3", "Psalm 100:4-5", "Psalm 118:24", "Psalm 119:11", "Proverbs 1:7", "Proverbs 3:5-6", "Proverbs 15:1", "Isaiah 9:6", "Isaiah 26:3", "Isaiah 53:5-6", "Jeremiah 29:11", "Matthew 6:33", "Matthew 28:18-20", "Luke 2:10-12", "John 1:1", "John 1:14", "John 3:16", "John 14:1-3", "John 14:6", "John 14:27", "Acts 1:8", "Acts 1:11", "Romans 1:16-17", "Romans 3:23", "Romans 5:8", "Romans 6:23", "Romans 8:28", "Romans 10:9-10", "Romans 12:1-2", "Romans 12:11-12", "Romans 12:18", "1 Corinthians 10:13", "2 Corinthians 9:7", "Galatians 2:20", "Galatians 5:22-23", "Ephesians 2:8-10", "Ephesians 5:18-21", "Philippians 4:4-7", "Colossians 3:15-17", "1 Thessalonians 5:16-18", "2 Timothy 3:16", "Hebrews 4:12", "Hebrews 4:16", "Hebrews 10:25", "Hebrews 11:6", "James 1:2-5", "1 Peter 1:3", "1 Peter 5:7", "1 John 1:7", "1 John 1:9", "1 John 5:11-12", "1 John 5:14-15", "Revelation 4:11", "Revelation 21:1-2", "Revelation 22:20"];
 var core_values_verses = [["Psalm 119:105", "CONNECTING WITH GOD\nThe Bible as God\u2019s word and as our highest authority"], ["2 Tim 3:16-17", "CONNECTING WITH GOD\nThe Bible as God\u2019s word and as our highest authority"], ["1 Tim 4:13-16", "CONNECTING WITH GOD\nBeing faithful in spiritual disciplines"], ["Psalm 1:2", "CONNECTING WITH GOD\nBeing faithful in spiritual disciplines"], ["2 Tim 2:15", "CONNECTING WITH GOD\nBeing faithful in spiritual disciplines"], ["Eph 2:8-9", "CONNECTING WITH GOD\nPersonally experiencing and testifying to God\u2019s saving grace"], ["1 John 1:2-3", "CONNECTING WITH GOD\nPersonally experiencing and testifying to God\u2019s saving grace"], ["Acts 26:29", "CONNECTING WITH GOD\nPersonally experiencing and testifying to God\u2019s saving grace"], ["1 Thess 5:16-18", "CONNECTING WITH GOD\nDepending on God through prayer"], ["Phil 4:6", "CONNECTING WITH GOD\nDepending on God through prayer"], ["Eph 6:18", "CONNECTING WITH GOD\nDepending on God through prayer"], ["1 Cor 10:31", "CONNECTING WITH GOD\nDoing all that we do as acts of worship unto God"], ["1 Cor 15:58", "CONNECTING WITH GOD\nDoing all that we do as acts of worship unto God"], ["Romans 12:1", "CONNECTING WITH GOD\nDoing all that we do as acts of worship unto God"], ["Matt 5:16", "GROWING UP\nMaturing in godly character"], ["1 Tim 4:7-8", "GROWING UP\nMaturing in godly character"], ["Romans 13:14", "GROWING UP\nMaturing in godly character"], ["Gal 5:13-14", "GROWING UP\nGrowing in love and service for others"], ["Eph 5:1-2", "GROWING UP\nGrowing in love and service for others"], ["1 John 3:18", "GROWING UP\nGrowing in love and service for others"], ["Eph 4:11-13", "GROWING UP\nEmbracing the role of spiritual leaders for discipleship"], ["2 Tim 2:2", "GROWING UP\nEmbracing the role of spiritual leaders for discipleship"], ["Heb 13:7", "GROWING UP\nEmbracing the role of spiritual leaders for discipleship"], ["Heb 13:17", "GROWING UP\nEmbracing the role of spiritual leaders for discipleship"], ["Matt 28:18-20", "GROWING UP\nEmbracing the role of spiritual leaders for discipleship"], ["James 2:17", "LIVING IT OUT\nLiving out scriptural values and mandates in all areas of life"], ["1 John 5:2-3", "LIVING IT OUT\nLiving out scriptural values and mandates in all areas of life"], ["Luke 9:57-62", "LIVING IT OUT\nLiving out scriptural values and mandates in all areas of life"], ["Luke 14:25-27", "LIVING IT OUT\nLiving out scriptural values and mandates in all areas of life"], ["1 Cor 10:31", "LIVING IT OUT\nLiving out scriptural values and mandates in all areas of life"], ["Acts 2:46-47", "LIVING IT OUT\nForming a biblical, countercultural community"], ["Romans 12:2", "LIVING IT OUT\nForming a biblical, countercultural community"], ["Eph 5:8-11", "LIVING IT OUT\nForming a biblical, countercultural community"], ["Heb 12:1", "LIVING IT OUT\nHonoring and emulating exemplary Christians \u2013 historical and contemporary"], ["1 Peter 5:9", "LIVING IT OUT\nHonoring and emulating exemplary Christians \u2013 historical and contemporary"], ["1 Cor 11:1", "LIVING IT OUT\nHonoring and emulating exemplary Christians \u2013 historical and contemporary"], ["Phil 2:29", "LIVING IT OUT\nHonoring and emulating exemplary Christians \u2013 historical and contemporary"], ["Joshua 4:4-7", "LIVING IT OUT\nLeaving a spiritual legacy for the next generation"], ["2 Tim 1:13-14", "LIVING IT OUT\nLeaving a spiritual legacy for the next generation"], ["Matt 26:6-13", "GIVING IT ALL\nLiving a passionate and sacrificial life"], ["Romans 12:11", "GIVING IT ALL\nLiving a passionate and sacrificial life"], ["2 Cor 11:23-29", "GIVING IT ALL\nLiving a passionate and sacrificial life"], ["2 Tim 4:6-7", "GIVING IT ALL\nLiving a passionate and sacrificial life"], ["Matt 6:19-20", "GIVING IT ALL\nGenerosity in giving time, money, talent and energy"], ["2 Cor 8:2-3", "GIVING IT ALL\nGenerosity in giving time, money, talent and energy"], ["2 Cor 9:6-7", "GIVING IT ALL\nGenerosity in giving time, money, talent and energy"], ["Acts 20:34-35", "GIVING IT ALL\nWorking hard and doing our best"], ["Col 3:23-24", "GIVING IT ALL\nWorking hard and doing our best"], ["Romans 12:5", "GETTING CLOSE\nDoing life together in all its ups and downs"], ["Gal 6:2", "GETTING CLOSE\nDoing life together in all its ups and downs"], ["Heb 3:13", "GETTING CLOSE\nDoing life together in all its ups and downs"], ["Acts 2:44-46", "GETTING CLOSE\nHaving open homes and open lives"], ["Acts 20:18", "GETTING CLOSE\nHaving open homes and open lives"], ["Romans 12:13", "GETTING CLOSE\nHaving open homes and open lives"], ["Acts 20:20", "GETTING CLOSE\nRelating to each other in love, covenantal commitment and honesty"], ["31", "GETTING CLOSE\nRelating to each other in love, covenantal commitment and honesty"], ["Romans 16:3-13", "GETTING CLOSE\nRelating to each other in love, covenantal commitment and honesty"], ["Eph 4:15", "GETTING CLOSE\nRelating to each other in love, covenantal commitment and honesty"], ["Heb 10:24-25", "GETTING CLOSE\nRelating to each other in love, covenantal commitment and honesty"], ["Ezra 7:10", "TRAINING UP\nKnowing and studying the word of God"], ["1 Tim 4:13", "TRAINING UP\nKnowing and studying the word of God"], ["2 Tim 4:2", "TRAINING UP\nKnowing and studying the word of God"], ["1 Peter 2:2", "TRAINING UP\nKnowing and studying the word of God"], ["Romans 12:2", "TRAINING UP\nTraining our minds to think from a Christian perspective in all areas of life"], ["Col 1:16", "TRAINING UP\nTraining our minds to think from a Christian perspective in all areas of life"], ["Eph 4:17-18", "TRAINING UP\nTraining our minds to think from a Christian perspective in all areas of life"], ["2 Cor 10:5", "TRAINING UP\nTraining our minds to think from a Christian perspective in all areas of life"], ["1 Peter 3:15", "TRAINING UP\nBeing able to articulate our faith"], ["Acts 26:25", "TRAINING UP\nBeing able to articulate our faith"], ["Exodus 35:10", "TRAINING UP\nDeveloping our gifts and skills"], ["Romans 12:6-8", "TRAINING UP\nDeveloping our gifts and skills"], ["1 Cor 14:12", "TRAINING UP\nDeveloping our gifts and skills"], ["1 Peter 4:10", "TRAINING UP\nDeveloping our gifts and skills"], ["Matt 28:19-20", "TRAINING UP\nRaising up leaders and ministers"], ["Col 1:28-29", "TRAINING UP\nRaising up leaders and ministers"], ["2 Tim 2:2", "TRAINING UP\nRaising up leaders and ministers"], ["Eph 4:11-13", "TRAINING UP\nRaising up leaders and ministers"], ["Acts 11:20", "REACHING OUT\nEquipping and sending out church planting teams"], ["Acts 13:2-3", "REACHING OUT\nEquipping and sending out church planting teams"], ["2 Tim 2:2", "REACHING OUT\nEquipping and sending out church planting teams"], ["Ezekiel 34:16", "REACHING OUT\nSharing God\u2019s heart for the lost"], ["Matt 9:36-38", "REACHING OUT\nSharing God\u2019s heart for the lost"], ["Matt 18:14", "REACHING OUT\nSharing God\u2019s heart for the lost"], ["Luke 19:10", "REACHING OUT\nSharing God\u2019s heart for the lost"], ["Acts 1:8", "REACHING OUT\nCreative and effective evangelism here and abroad"], ["1 Cor 9:22-23", "REACHING OUT\nCreative and effective evangelism here and abroad"], ["Matt 18:5", "REACHING OUT\nCompassionate service to the needy"], ["Matt 25:40", "REACHING OUT\nCompassionate service to the needy"], ["James 1:27", "REACHING OUT\nCompassionate service to the needy"]];
+
 
 //Add last chunk of passage based on group
 //Add rate limit based on time
@@ -49,6 +56,7 @@ var core_values_verses = [["Psalm 119:105", "CONNECTING WITH GOD\nThe Bible as G
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       verseRegex = /^\/verse.*$/i,
+      helpRegex = /^\/h(elp)?\s?$/i,
       dtRegex = /^\/dt\s?$/i,
       proverbRegex = /^\/proverbs?\s?$/i,
       romansRegex = /^\/romans\s?$/i,
@@ -99,6 +107,10 @@ function respond() {
    } else if (hangoutRegex.test(request.text) && request.group_id === "15516149") {
     this.res.writeHead(200);
      postMessageVerse(hangout_question, request.group_id);
+     this.res.end();
+   } else if (helpRegex.test(request.text)) {
+    this.res.writeHead(200);
+     postMessageVerse(help_messagep, request.group_id);
      this.res.end();
    } else if (rest_of_passage_dict[request.group_id].length > 0 && request.text === last_chunk_dict[request.group_id]) {
      this.res.writeHead(200);
@@ -505,6 +517,5 @@ function postMessage() {
   });
   botReq.end(JSON.stringify(body));
 }
-
 
 exports.respond = respond;
